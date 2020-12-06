3346758a1cc8042b4de7b9dd12e338e52d671d7f
Brucei <2169799706@qq.com>
2020-12-06 21:55:03 +0800

    xiugai

--- decider.v
+++ decider.v
@@ -28,58 +28,66 @@ input Valid_1,      //鏈夋晥淇″彿
 //input [15:0] Key_1,
 //input  [3:0] Key,
 input clk,
+input set,
                                     //瀵瑰簲瀛樺偍鍣ㄧ殑鍦板潃
 //input [2:0] RAM_1_addr,            //瀵瑰簲瀛樺偍鍣ㄧ殑鍦板潃
 output reg OPEN,
 output reg  LOCK,
 output reg SAVE_LIGHT,
   output   reg  [2:0] count_1,
-  output reg [3:0] RAM_1_DATA,
+  output reg [3:0] RAM_1_DATA ,
   output reg [3:0] RAM_DATA
     );  
     integer i;    
-    reg [1:0] RAM_addr_1;
-    reg [2:0] RAM_addr;
+    reg [3:0] RAM_addr_1;
+    reg [9:0] RAM_addr;
  // output   wire [3:0] RAM_DATA_1;         
  // output   wire [3:0]  RAM_DATA;                                              
     reg [2:0] state_1,next_state_1;
- reg [3:0] RAM [7:0];
+ reg [3:0] RAM [9:0];
   reg [3:0] RAM_1 [3:0];
-    initial $readmemb("RAM_DATA.txt",RAM_1,0,3);
+  //  initial $readmemb("RAM_DATA.txt",RAM_1,0,3);
     parameter B_0=4'b0001;   //LOCK
     parameter B_1=4'b0010;   //OPEN THE LOCK
     parameter B_2=4'b0100;   //SAVE
  //   parameter B_3=4'b1000;   //LOCK
 
- always @(posedge clk)
+ initial
  begin
- if(reset_1==0)
- RAM_addr_1=0;
- else if(RAM_addr_1<=3)              //存储输入RAM地址
- begin
-RAM_addr_1=RAM_addr_1+1;
-RAM_1_DATA=RAM_1[RAM_addr_1];
-end
-else 
-RAM_addr_1=0;
-end
+ RAM_1[1]=4'b1001;
+  RAM_1[2]=4'b1100;
+   RAM_1[4]=4'b0001;
+    RAM_1[8]=4'b0000;
+ end
 
- always @(posedge clk)     //存储密码RAM地址
+ always @(posedge clk)     //存储输入RAM地址
  begin
- if(reset_1==0)
- RAM_addr_1=0;
- else if(RAM_addr<=15)
+ if(reset_1==1)
+ RAM_addr_1=10'b0000000001;
+ else if(RAM_addr<=512)
  begin
-RAM_addr=RAM_addr+1;
+RAM_addr=RAM_addr<<1;
 RAM[RAM_addr]=Code_1;
 RAM_DATA=RAM[RAM_addr];
 end
 else 
-RAM_addr=0;
+RAM_addr=10'b00000001;
+end
+ always @(posedge clk)     //存储输入RAM地址
+ begin
+ if(reset_1==1)
+ RAM_addr_1=4'b0001;
+ else if(RAM_addr_1<=8)
+ begin
+RAM_addr_1=RAM_addr_1<<1;
+RAM_1[RAM_addr_1]=4'b0001;
+
+end
+else 
+RAM_addr=4'b0001;
 end
 
-
-
+/*
    always@(posedge clk )            //###############
    begin
    if(reset_1)                                   //杩涘叆澶嶄綅锛屽彲浠ヨ緭鍏ユ柊瀵嗙爜
@@ -87,31 +95,35 @@ end
    else 
    state_1<=next_state_1;      
    end                       //鍚﹀垯杩涘叆涓嬩竴鐘舵??
-
-always@(state_1,Valid_1,Code_1)
+*/
+always@(posedge clk)
     begin
-    if(reset_1==0)
+    if(reset_1==1)
+    begin
     OPEN=1'b0;
     SAVE_LIGHT=1'b0;
     LOCK=1'b1;
+    end
+    else 
+    begin
                 case(state_1)
 B_0:                                                        //空闲状态，判断是否按下密码和"#"键，按下则跳转至开启状态
-
-                    if((RAM[1]==RAM_1[0])&&(RAM[2]==RAM_1[1])&&(RAM[3]==RAM_1[2])&&(RAM[4]==RAM_1[3])&&(RAM[0]==4'b1011))
+begin
+                    if((RAM[2]==RAM_1[1])&&(RAM[4]==RAM_1[2])&&(RAM[8]==RAM_1[4])&&(RAM[16]==RAM_1[8])&&(RAM[1]==4'b1011))
                         begin
                             next_state_1=B_1;
                             OPEN=1'b1;
                             SAVE_LIGHT=1'b0;
                             LOCK=1'b0;
                         end
-                    else if(RAM[0]==4'b1011)                  //涓?鐩存寜涓嬧??#閿?濓紝鍒欎竴鐩存墦寮?
+                    else if(RAM[1]==4'b1011)                  //涓?鐩存寜涓嬧??#閿?濓紝鍒欎竴鐩存墦寮?
                         begin
                         next_state_1=B_1;                  
                         OPEN=1'b1;
                         SAVE_LIGHT=1'b0;
                         LOCK=1'b0;
                         end
-                        else if((RAM[1]==RAM_1[1])&&(RAM[2]==RAM_1[2])&&(RAM[3]==RAM_1[3])&&(RAM[4]==RAM_1[4])&&(RAM[0]==4'b1010))
+                        else if((RAM[2]==RAM_1[1])&&(RAM[4]==RAM_1[2])&&(RAM[8]==RAM_1[4])&&(RAM[16]==RAM_1[8])&&(RAM[1]==4'b1010))
                         begin
 
                         next_state_1=B_2;                 //进入存储状态
@@ -126,14 +138,15 @@ B_0:                                                        //空闲状态，判断是否
                          OPEN=1'b0;
                           SAVE_LIGHT=1'b0;
                         end
-                        
-B_1:                                                               //打开状态
-if((reset_1==0)&&(Valid_1==1))
+        end                
+B_1: 
+begin                                                              //打开状态
+if(Valid_1==1)
 begin
              OPEN=1'b1;
              LOCK=1'b0;
              SAVE_LIGHT=1'b0;
-             if(RAM[0]==4'b1011)                            //一直按下”#“保持打开
+             if(RAM[1]==4'b1011)                            //一直按下”#“保持打开
              begin
              next_state_1=B_1;
              OPEN=1'b1;
@@ -163,11 +176,13 @@ begin
 
                         end
 end
+end
 B_2:                      //存储状态
-if(reset_1==1)               //进入复位，输入4位密码
 begin
-for(i=0;i<4;i=i+1)
-RAM[i]=Code_1;
+if(set==1)               //进入复位，输入4位密码
+begin
+for(i=0;i<8;i=i*2)
+RAM_1[i]=Code_1;
 end
 else
 begin                          //进入存储
@@ -175,27 +190,31 @@ count_1=1'b0;
 SAVE_LIGHT=1'b1;
  LOCK=1'b1;
  OPEN=1'b0;
-for(i=0;i<10;i=i+1)                    //输入两遍密码
+for(i=1;i<=512;i=i*2)                    //输入两遍密码
 begin
 RAM[i]=Code_1;
 end
 
-while (!((RAM[1]&RAM[6])&&(RAM[2]&RAM[7])&&(RAM[3]&RAM[8]))&&(RAM[4]&RAM[9])&&(RAM[0]&4'b1011)&&(RAM[4]&4'b1011))   //若两遍输入不同，重新输入。每次输入都以"#"结束
+while (!((RAM[2]==RAM[64])&&(RAM[4]==RAM[128])&&(RAM[8]==RAM[256]))&&(RAM[16]==RAM[512])&&(RAM[1]==4'b1011)&&(RAM[32]==4'b1011))   //若两遍输入不同，重新输入。每次输入都以"#"结束
 begin
-for(i=0;i<10;i=i+1)
+for(i=1;i<=512;i=i+1)
 begin
 RAM[i]=Code_1;
 count_1=count_1+1;
 end
 end
-
+RAM_1[1]=RAM[2];
+RAM_1[2]=RAM[4];
+RAM_1[4]=RAM[8];
+RAM_1[8]=RAM[16];
 SAVE_LIGHT=1'b0;   //修改完密码，存储指示灯灭
  LOCK=1'b1;
  OPEN=1'b0;
  next_state_1=B_0;  //修改完密码进入锁定状态
 end
-default: next_state_1=B_0;   //默认进入锁定状态
+end
 endcase
 
+end
 end
 endmodule
