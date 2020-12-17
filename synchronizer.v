b2a30c86c9e583d7dc504631d982a7fa96c4a34c
Brucei <2169799706@qq.com>
2020-12-17 21:42:29 +0800

    去抖

--- synchronizer.v
+++ synchronizer.v
@@ -3,8 +3,32 @@ module Synchronizer(
     input               clock,
     input               reset,
     
-    output              S_Row
+    output        reg      S_Row
 );
-
-assign S_Row = (Row[0]||Row[1]||Row[2]||Row[3]);
+reg [23:0] cnt;
+localparam TIME=24'h1E8480;
+reg key_cnt;
+always @(posedge clock or negedge reset) begin
+        if(reset == 0)
+            S_Row <= 0;
+        else if(cnt == TIME - 1)
+            S_Row<= (Row[0]||Row[1]||Row[2]||Row[3]);
+    end
+	always @(posedge clock or negedge reset) begin
+        if(reset == 0)
+            cnt <= 0;
+        else if(key_cnt)
+            cnt <= cnt + 1'b1;
+        else
+            cnt <= 0; 
+    end
+	always @(posedge clock or negedge reset) begin
+            if(reset == 0)
+                key_cnt <= 0;
+            else if(key_cnt == 0 && Row != S_Row)
+                key_cnt <= 1;
+            else if(cnt == TIME - 1)
+                key_cnt <= 0;
+     end
+//assign S_Row = (Row[0]||Row[1]||Row[2]||Row[3]);
 endmodule
