9c2e959c1f2d84d16508b5ffae50861a21d6fa4b
ThisIsMarxMA <ma376371893@163.com>
2021-08-13 14:41:11 +0800

    test

--- control.v
+++ control.v
@@ -147,7 +147,6 @@ module control(
 
    assign confirm = key_num==10 && key_vld;
 
-
     always  @(posedge clk or negedge rst_n)begin
         if(rst_n==1'b0)begin
            password_correct_twice <= 0;
